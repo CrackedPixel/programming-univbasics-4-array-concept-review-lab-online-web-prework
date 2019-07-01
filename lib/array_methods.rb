924917888e7a4ad6bf4bdae8a484fd155329ff0b
Anthony Carbajal <acarbajal721@live.com>
2019-07-01 23:10:23 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,5 @@
 def find_element_index(array, value_to_find)
-  indexNum = array.include?(value_to_find)
-  return indexNum == true ? indexNum : nil
+  return array.index(value_to_find)
 end
 
 def find_max_value(array)
